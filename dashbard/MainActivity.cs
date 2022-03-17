503097631b67c12a6cb706035c1b3ab7420d14c8
Hp <Hp@DESKTOP-H5VIABE>
2022-03-17 21:30:50 +0530

    Add project files.

---
+++ dashbard/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using dashbard.Resources;
+using static Google.Android.Material.BottomNavigation.BottomNavigationView;
+
+namespace dashbard
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnNavigationItemSelectedListener
+    {
+        private dashFragment _dashboardFragment;
+        private locationFragment _locationFragment;
+        private favFragment _favouriteFragment;
+        
+        private personsFragment _personFragment;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            _dashboardFragment = new dashFragment();
+            _locationFragment = new locationFragment();
+            _favouriteFragment=new favFragment();
+            _personFragment=new personsFragment();
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.home:
+
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayout, _dashboardFragment).Commit();
+                    break;
+                case Resource.Id.location:
+
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayout, _locationFragment).Commit();
+                    break;
+                case Resource.Id.favourite:
+
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayout, _favouriteFragment).Commit();
+                    break;
+                case Resource.Id.person:
+
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayout, _personFragment).Commit();
+                    break;
+
+            }
+            return true;
+        }
+    }
+}
---
+++ dashbard/dashFragment.cs
@@ -0,0 +1,45 @@
+﻿using Fragment = AndroidX.Fragment.App.Fragment;
+using Android.Content;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dashbard
+{
+    public class dashFragment : Fragment
+    {
+        private Context context;
+
+        public dashFragment()
+        {
+        }
+
+        public dashFragment(Context context)
+        {
+            this.context = context;
+        }
+
+        
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            View view = inflater.Inflate(Resource.Layout.layout2, container, false);
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
